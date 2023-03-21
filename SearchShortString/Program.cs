9ea6954aee3a0608a1d8f0178ced8fe8fb6e8d2b
Ivan Uvarov <uvarov_ivan@rambler.ru>
2023-03-21 14:30:40 +0300

    Added inputing

---
+++ SearchShortString/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int InputInt(string msg)
+{
+    System.Console.Write($"Введите {msg} >");
+    return (Convert.ToInt32(Console.ReadLine()));
+}
+
+string InputStr(string msg)
+{
+    System.Console.WriteLine($"Введите {msg} >");
+    return (Console.ReadLine());
+}
+
+
